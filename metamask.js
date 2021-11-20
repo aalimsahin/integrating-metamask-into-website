20e1a315ce8f8813226e5221f1080d35d270ace7
aalimsahin <alimsahin0007@gmail.com>
2021-11-20 15:55:35 +0300

    integrate balance

--- metamask.js
+++ metamask.js
@@ -5,12 +5,23 @@ const connectMetaMask = async () => {
     .catch((err) => {
       console.log(err.code);
     });
+
+  let balance = await window.ethereum.request({
+    method: "eth_getBalance",
+    params: [accounts[0]]
+  }).catch((err) => {
+    console.log(err);
+  })
+  balance = parseInt(balance);
+  balance = balance/Math.pow(10,18);
+  console.log(balance);
+
   let header = document.getElementById("header");
   let footer = document.getElementById("footer");
   let img = document.getElementById("img");
   img.src = "images/metamask-green.png";
-    footer.innerHTML =
-    " Active Account : " + accounts[0];
-    footer.style.setProperty("background-color", "#D4EDDA");
-    header.style.setProperty("background-color", "#D4EDDA");  
+  footer.innerHTML =
+  " Active Account : "+ accounts[0].slice(0,7)+"..."+ accounts[0].slice(-6) +"<br>"+ "Balance: "+Number.parseFloat(balance).toFixed(4) + " ETH";
+  footer.style.setProperty("background-color", "#D4EDDA");
+  header.style.setProperty("background-color", "#D4EDDA");
 };
